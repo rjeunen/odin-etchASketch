318556f4ed85946540826736f2ef4fb1fd1c1b8f
Robby Jeunen <robbyjeunen@gmail.com>
2026-09-16 21:26:37 +0200

    Added button and logic to change the grid size

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 
 //Elements
 const container = document.querySelector("#container");
+const gridSizeButton = document.querySelector("#gridSize");
 
 //Global variables
 let gridSize = 16;
 let celWidthAndHeigth = 960 / gridSize;
 let pixels = `${celWidthAndHeigth}px`;
+
 
 //function to create the 16 by 16 grid
 function createGrid(){
@@ -17,13 +19,39 @@ function createGrid(){
             const gridCel = document.createElement("div");
             gridCel.style.width = pixels;
             gridCel.style.height = pixels;
-            gridCel.style.border = `1px solid black`;
+            //gridCel.style.border = `1px solid black`;
             
             gridCel.addEventListener('mouseenter', () => {
                 gridCel.classList.add('hoverEffect');
             });
             container.appendChild(gridCel);
         }
+    }
+}
+
+//Remake grid with the size the user wants
+gridSizeButton.addEventListener('click', () => {
+    deleteGrid();
+    
+    // let wantedGridSize = prompt(`What grid size would you like?`);
+    // if(wantedGridSize > 100){
+    //     wantedGridSize = prompt(`the grid size should not be greater then 100, enter another number`);
+    // }
+
+    let wantedGridSize = prompt(`What grid size would you like?`);
+    while (Number(wantedGridSize) > 100){
+        wantedGridSize = prompt(`The grid size can not be greater then 100`);
+    };
+    gridSize = Number(wantedGridSize);
+    celWidthAndHeigth = 960 / gridSize;
+    pixels = `${celWidthAndHeigth}px`;
+    createGrid();
+});
+
+//delete grid
+function deleteGrid(){
+    for(let i = container.children.length - 1; i >= 0; i--){
+        container.removeChild(container.children[i]);
     }
 }
 
